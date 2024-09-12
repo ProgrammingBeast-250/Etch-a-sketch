c9b2da2b8529e352bc28d09d6d03f687462b8851
Programming-Beast1 <67921050+WolfBoy250@users.noreply.github.com>
2024-09-12 12:39:06 +0100

    Completed project

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 for (let i = 1; i <= 16;i++) {
     let Div = document.createElement('div')
     let thing = document.body.appendChild(Div)
-    Div.id = "[" + i + ", 0]"
+    Div.id = "column"
     for (let j = 1; j <= 16;j++) {
         let Div = document.createElement('div')
         thing.appendChild(Div)
-        Div.id = "[" + i + ", " + j + "]"
+        Div.id = "row"
+        Div.textContent = "."
+        Div.addEventListener("click", function () {
+            if (Div.style.backgroundColor === "red"){
+            Div.style.backgroundColor = "lightgrey"
+            }
+            else {
+                Div.style.backgroundColor = "red"
+            }
+        })
     }
 }
